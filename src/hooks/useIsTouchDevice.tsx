fb49f0b2460e1bfb389183657060b03ce0c55bdb
interc0der <intercoder@whirled.io>
2022-08-29 02:21:28 -0700

    Add: react hooks to determine if on mobile or desktop

---
+++ src/hooks/useIsTouchDevice.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+
+const isTouchDevice = () => {
+  if (typeof window === 'undefined') return false;
+  const prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+
+  const mq = (query: string) => {
+    return typeof window !== 'undefined' && window.matchMedia(query).matches;
+  };
+
+  // @ts-ignore
+  if ('ontouchstart' in window || (window?.DocumentTouch && document instanceof DocumentTouch))
+    return true;
+
+  const query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join(''); // include the 'heartz' - https://git.io/vznFH
+
+  return mq(query);
+};
+
+export const useIsTouchDevice = () => {
+  const [isTouch, setIsTouch] = useState<boolean>(false);
+  useEffect(() => {
+    const {
+      isAndroid,
+      isIPad13,
+      isIPhone13,
+      isWinPhone,
+      isMobileSafari,
+      isTablet,
+    } = require('react-device-detect');
+    setIsTouch(
+      isTouch ||
+        isAndroid ||
+        isIPad13 ||
+        isIPhone13 ||
+        isWinPhone ||
+        isMobileSafari ||
+        isTablet ||
+        isTouchDevice()
+    );
+  }, []);
+
+  return isTouch;
+};
+
+export default useIsTouchDevice;
