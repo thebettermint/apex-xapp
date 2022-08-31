be37ccc531af30abeee956806f0aa01d279a1ad6
interc0der <intercoder@whirled.io>
2022-08-31 07:46:58 -0700

    Update: debugging production error with metadata path

--- src/pages/api/xumm/meta/[uuid].ts
+++ src/pages/api/xumm/meta/[uuid].ts
@@ -6,6 +6,12 @@ import { reqApiKeyMatch } from '@/api/middleware/xumm.middleware';
 const handler = async (req: ExtendedNextApiRequest, res: NextApiResponse) => {
   const { method } = req;
 
+  console.log('... server request, finding metadata for payload');
+  const query = req.query;
+  console.log(query);
+  let { uuid } = query;
+  console.log(uuid);
+
   if (method == 'GET') {
     let meta = await xummController.getPayloadMetadata(req, res);
     if (meta) return;
