a44588009a37fbf3d3eb239a1507c24a3fe06ae4
interc0der <intercoder@whirled.io>
2022-08-31 15:15:12 -0700

    debugging window, add headers, remove xapp context, check window

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,23 +7,35 @@ import XAppContextProvider from 'src/context/xapp';
 
 import Header from 'src/components/Header';
 
+import React, { useState, useEffect } from 'react';
+
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <script
-        data-consolejs-channel="7ec8c9da-257b-4e52-9918-453a2cef4aa4"
-        src="https://remotejs.com/agent/agent.js"></script>
-      <StoreProvider>
-        <div className={styles.main}>
-          <Header />
-          <Component {...pageProps} />
-        </div>
-      </StoreProvider>
+      {domLoaded && (
+        <>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </Head>
+          <script
+            data-consolejs-channel="7ec8c9da-257b-4e52-9918-453a2cef4aa4"
+            src="https://remotejs.com/agent/agent.js"></script>
+          <StoreProvider>
+            <div className={styles.main}>
+              <Header />
+              <Component {...pageProps} />
+            </div>
+          </StoreProvider>
+        </>
+      )}
     </>
   );
 }
