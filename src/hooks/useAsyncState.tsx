9f5257fcf1311b3651f2f8446a576daefa27a567
interc0der <intercoder@whirled.io>
2022-08-30 19:51:48 -0700

    Add: async setState hook if needed

---
+++ src/hooks/useAsyncState.tsx
@@ -0,0 +1,34 @@
+import { useState, useRef, useCallback, useEffect } from 'react';
+
+function useAsyncState(initialState: any) {
+  const [state, setState] = useState(initialState);
+  const resolveState = useRef<any>();
+  const isMounted = useRef<boolean>(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (resolveState.current) resolveState.current(state);
+  }, [state]);
+
+  const setAsyncState = useCallback(
+    (newState: any) =>
+      new Promise((resolve) => {
+        if (isMounted.current) {
+          resolveState.current = resolve;
+          setState(newState);
+        }
+      }),
+    []
+  );
+
+  return [state, setAsyncState];
+}
+
+export default useAsyncState;
