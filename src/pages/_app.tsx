d3f336b4d70adada1c0a3d57436058fb7f78a089
interc0der <intercoder@whirled.io>
2022-08-31 15:41:58 -0700

    debugging window

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,39 +3,26 @@ import styles from '../styles/index.module.scss';
 import type { AppProps } from 'next/app';
 
 import StoreProvider from '../context/store';
-import XAppContextProvider from 'src/context/xapp';
 
 import Header from 'src/components/Header';
-
-import React, { useState, useEffect } from 'react';
 
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [domLoaded, setDomLoaded] = useState(false);
-
-  useEffect(() => {
-    setDomLoaded(true);
-  }, []);
-
   return (
     <>
-      {domLoaded && (
-        <>
-          <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          </Head>
-          <script
-            data-consolejs-channel="7ec8c9da-257b-4e52-9918-453a2cef4aa4"
-            src="https://remotejs.com/agent/agent.js"></script>
-          <StoreProvider>
-            <div className={styles.main}>
-              <Header />
-              <Component {...pageProps} />
-            </div>
-          </StoreProvider>
-        </>
-      )}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <script
+        data-consolejs-channel="7ec8c9da-257b-4e52-9918-453a2cef4aa4"
+        src="https://remotejs.com/agent/agent.js"></script>
+      <StoreProvider>
+        <div className={styles.main}>
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </StoreProvider>
     </>
   );
 }
