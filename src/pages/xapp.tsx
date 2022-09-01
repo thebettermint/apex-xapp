fa0a423df4b50e2c6fce2c1a046a713d1b6411b6
interc0der <intercoder@whirled.io>
2022-09-01 07:15:47 -0700

    Try useEffect mounted hook on /xapp route

--- src/pages/xapp.tsx
+++ src/pages/xapp.tsx
@@ -46,20 +46,6 @@ const XApp: NextPage = () => {
     Router.push('/signup');
   };
 
-  const [hasMounted, setHasMounted] = useState(false);
-
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
-
-  if (!hasMounted) {
-    return (
-      <>
-        <div>Loading...</div>
-      </>
-    );
-  }
-
   useEffect(() => {
     console.log('is mobile :', mobileDetect.isMobile());
     console.log('is style :', style);
