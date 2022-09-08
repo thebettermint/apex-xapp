67c944e68b66a275e5639a34761a9b4e460e4f33
interc0der <intercoder@whirled.io>
2022-09-07 18:20:40 -0700

    Update: remove remotejs debugger

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,11 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <script
-        data-consolejs-channel="7ec8c9da-257b-4e52-9918-453a2cef4aa4"
-        src="https://remotejs.com/agent/agent.js"
-        defer
-      />
+
       <StoreProvider>
         <div className={styles.main}>
           <Header />
